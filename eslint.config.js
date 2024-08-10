8dff7e6099e806accd197539b3e3427c8d4d4068
Anh Tung Bui <anhtung.bui@gmail.com>
2024-08-10 15:57:25 +0200

    chore: add custom rules to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,20 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
+const customRules = {
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+}
+
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       globals: globals.browser,
     },
-    rules: {},
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  customRules,
 ]
